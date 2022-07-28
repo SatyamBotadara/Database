d7bd791575ccb502a94ff1edd7fcb7e5216ad40a
satyam <satyambotadara99m@gmail.com>
2022-07-28 12:50:49 +0530

    commit

--- app/src/main/java/com/example/database/sqlite/dashboard.java
+++ app/src/main/java/com/example/database/sqlite/dashboard.java
@@ -1,17 +1,16 @@
-package com.example.database;
+package com.example.database.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.database.R;
+
+public class dashboard extends AppCompatActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboard);
     }
 }
-
-
